01891d5823ae4c5aef21569cbb8f4ee43fcf6e2a
dec0mpiled <liimbix@gmail.com>
2018-11-24 03:00:24 -0500

    working-build

--- app.js
+++ app.js
@@ -1,41 +1,57 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
+var hbs = require('hbs');
+var mongoose = require('mongoose');
+var http = require('http').Server(app);
+var app = express();
+var server = app.listen(process.env.PORT);
+var io = require('socket.io').listen(server);
+
+
+var Message = mongoose.model('Message',{ name : String, message : String});
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://dec0mpiled:welcometor4ge@ds023480.mlab.com:23480/chatapp', { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("SUCCESS: Connected to the Database. Connect at: " + server.address().port);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use('/', indexRouter);
+app.use(express.static(path.join(__dirname, 'public')));
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  });
+});
+
+app.post('/messages', (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) =>{
+    if(err)
+      res.sendStatus(500);
+      io.emit('message', req.body);
+    res.sendStatus(200);
+  });
+});
+
+
+io.on('connection', function(client) {  
+    console.log('Client connected or page refreshed..');
 });
 
 module.exports = app;
