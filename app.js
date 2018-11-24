da5c30fcff5992130b54a655d3430558d5171686
dec0mpiled <liimbix@gmail.com>
2018-11-24 04:37:30 -0500

    heroku plz

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 5000);
 
 
 var Message = mongoose.model('Message',{ name : String, message : String});
